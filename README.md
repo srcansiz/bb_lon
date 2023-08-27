a986e498e87728e20308992d769c2ebf427e6f6a
Sergen Cansız <sergencansiz@gmail.com>
2023-08-27 15:53:37 +0200

    webpack setup babylonjs

---
+++ README.md
@@ -0,0 +1,4 @@
+```
+npm install --save-dev typescript webpack ts-loader webpack-cli
+npm install --save @babylonjs/core @babylonjs/gui babylonjs-materials  
+```
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+
+
+const path = require('path')
+const fs = require("fs");
+const appDirectory = fs.realpathSync(process.cwd());
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: "./src/app.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['.ts', 'tsx', '.js']
+    },
+    devServer: {
+        host: "0.0.0.0",
+        port: 8181, //port that we're using for local host (localhost:8080)
+        static: path.resolve(appDirectory, "public"), //tells webpack to serve from the public folder
+        hot: true,
+        devMiddleware: {
+            publicPath: "/",
+        }
+    },
+    devtool: 'source-map',
+    plugins: [],
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    }
+}
